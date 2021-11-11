d42e157a5913616b1e84e71da147461f1eba532e
DucAnh <daoducanhc@gmail.com>
2021-11-11 14:40:50 +0700

    test

--- musicbe/music/urls.py
+++ musicbe/music/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 app_name = 'music'
 urlpatterns = [
     path('song/', views.ListCreateSongView.as_view()),
@@ -9,4 +14,7 @@ urlpatterns = [
     path('song/detail/<int:pk>', views.DetailSongView.as_view()),
     path('song/search/title/<str:s>', views.SearchSongTitleView.as_view()),
     path('song/search/artist/<str:s>', views.SearchSongArtistView.as_view()),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
